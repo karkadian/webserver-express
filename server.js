dc45d72e97f396233b8a410e8e86a5f04e7b63b8
Kevin Carcache <karkadian@hotmail.es>
2020-01-29 22:33:28 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials( __dirname + '/views/partials' );
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        nombre: 'kevin'
+    });
+})
+app.get('/about', (req, res) => {
+    res.render('about.hbs');
+})
+
+app.listen(port);
